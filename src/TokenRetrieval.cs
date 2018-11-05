1d46cba74d37dbaef7f82a863e3e80b216b19177
Steffen Lehmann <steffen@stejin.org>
2018-11-05 13:10:27 -0800

    Changed token format

--- src/TokenRetrieval.cs
+++ src/TokenRetrieval.cs
@@ -44,46 +44,21 @@ namespace ProDerivatives.AsymmetricAuthentication
         /// </summary>
         /// <param name="scheme">The scheme (defaults to Bearer).</param>
         /// <returns></returns>
-        public static Func<HttpRequest, AuthenticationToken> FromAuthenticationHeader(string scheme = "Signature")
+        public static Func<HttpRequest, AuthenticationToken> FromAuthenticationHeader(string scheme = AsymmetricAuthenticationDefaults.AuthenticationScheme)
         {
             return (request) =>
             {
-                string authentication = request.Headers["Authentication"].FirstOrDefault();
+                string authentication = request.Headers[AsymmetricAuthenticationDefaults.AuthenticationScheme].FirstOrDefault();
 
                 if (string.IsNullOrEmpty(authentication))
                 {
                     return null;
                 }
 
-                if (authentication.StartsWith(scheme + " ", StringComparison.OrdinalIgnoreCase))
-                {
-                    var tokens = authentication.Split(',');
-                    string nonce = ExtractTokenValue(tokens, "Nonce");
-                    long nonceValue = 0;
-                    long.TryParse(nonce, out nonceValue);
+                var authenticationToken = Newtonsoft.Json.JsonConvert.DeserializeObject<AuthenticationToken>(authentication);
 
-                    var result = new AuthenticationToken()
-                    {
-                        Signature = ExtractTokenValue(tokens, "Signature"),
-                        PublicKey = ExtractTokenValue(tokens, "PublicKey"),
-                        Nonce = nonceValue
-                    };
-                    return result;
-                }
-
-                return null;
+                return authenticationToken;
             };
-        }
-
-        private static string ExtractTokenValue(string[] tokens, string token) {
-            foreach (var t in tokens) {
-                var currentToken = t.Trim();
-                if (currentToken.StartsWith(token, StringComparison.OrdinalIgnoreCase))
-                {
-                    return currentToken.Substring(token.Length + 1).Trim();
-                }
-            }
-            return String.Empty;
         }
 
     }
