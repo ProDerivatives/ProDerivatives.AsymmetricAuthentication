8a39759211ed4b1c5e2e58d11f469c4a6521acbd
Steffen Lehmann <steffen@stejin.org>
2018-11-05 03:28:03 -0800

    Updated Token

--- src/AuthenticationToken.cs
+++ src/AuthenticationToken.cs
@@ -6,10 +6,24 @@ namespace ProDerivatives.AsymmetricAuthentication
     /// </summary>
     public class AuthenticationToken
     {
+        /// <summary>
+        /// Signature
+        /// </summary>
         public string Signature { get; set; }
 
+        /// <summary>
+        /// Public Key
+        /// </summary>
         public string PublicKey { get; set; }
 
+        /// <summary>
+        /// Nonce
+        /// </summary>
         public long Nonce { get; set; }
+
+        /// <summary>
+        /// Timestamp
+        /// </summary>
+        public DateTime Timestamp { get; set; }
     }
 }
